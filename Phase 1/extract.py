ddd99cded86396888e0730c52561b571ebfc18e1
jacobkj314 <jacobkj314@gmail.com>
2022-02-07 18:27:50 -0700

    Created Skeleton

--- Phase 1/extract.py
+++ Phase 1/extract.py
@@ -1,6 +1,35 @@
-import predict
-from spacy import displacy
+import sys
+from predict import getNER, detectRelation
 
-ner = predict.getNER()
+#The main spacy model to hold the entities that will be used for extracting drugs
+ner = getNER()
 
-displacy.serve(predict.ner(), style = "dep")
+def __main__():
+    #This section of the code gets the document body from command line or a file
+    docText = ""
+    try:
+        docText = " ".join(sys.argv[1:])
+        docText = open(sys.argv[1], "r").read()
+    except:
+        pass
+
+    #the document is passed into the spacy model to extract drug entities
+    doc = ner(docText)
+
+    #extract all possible relations
+    relations = set()#a container to hold extracted relations
+    for s, sentence in enumerate(doc.sents):#the document is split into sentences
+        ents = sentence.ents#spacy extracts the entities
+        entCount = len(ents)
+        for i, first in enumerate(ents):
+            for second in [ents[j] for j in range(i + 1, entCount, 1)]:
+                relation = detectRelation(first, second, sentence)#each potential pair is compared
+                if relation is not None:
+                    relations.add((first.text, second.text, relation, s))#if there is a relation, add it to the extracted relations
+    
+    #format and print results
+    for first, second, relation, sentence in relations: 
+        print(first + ", " + second + ": " + relation + " (sentence " + str(sentence) + ")")
+
+if __name__ == "__main__":
+    __main__()
--- Phase 1/predict.py
+++ Phase 1/predict.py
@@ -1,66 +1,10 @@
-import os
-from xml.etree import ElementTree as ET
-import spacy
 import pickle
+from spacy.language import Language
+from spacy.tokens.span import Span
 
-def getDEV() -> list[set[tuple[int, int, str]]]:
-    return pickle.load(open("Dev/DEV", "rb"))
-dev = getDEV()
-
-def getNER() -> spacy.language.Language:
-    return pickle.load(open("NER", "rb")) 
+def getNER() -> Language:
+    return pickle.load(open("../NER", "rb")) 
 ner = getNER()
-def extractLabeledEntities(sentenceText: str) -> set[tuple[int, int, str]]:
-    entities = set()
-    sentence = ner(sentenceText)#convert text to spacy model
-    for ent in sentence.ents:
-        entities.add((ent.start_char, ent.end_char, ent.label_))
-    return entities
-def extractEntities(sentenceText: str) -> list[tuple[int, int]]: #this returns just the entities themselves, in order
-    return sorted({t[:2] for t in extractEntities(sentenceText)}, key = lambda t : t[0])
 
-
-"""
-sentenceList = list()
-docFolder = "Dev"
-for file in os.listdir(docFolder): #get every training document
-    if str(file) == "DEV":
-        continue
-    file = os.path.join(docFolder, file)
-    #doc = open(file, "r").read()
-    root = ET.parse(file).getroot() #parse document as XML tree
-    for sentence in root.iter("sentence"):
-        sentenceText = sentence.get("text")
-        sentenceList.append(sentenceText)
-
-
-
-extractedList = list()
-answers, guesses, hits = 0,0,0
-i = 0
-while i < len(dev) and i < len(sentenceList):
-    #gold = dev[i]
-    gold = {t[:2] for t in dev[i]}
-    #extractedEntities = extractEntities(sentenceList[i])
-    extractedEntities = {t[:2] for t in extractEntities(sentenceList[i])}
-    answers += len(gold)
-    guesses += len(extractedEntities)
-    hits += len(gold.intersection(extractedEntities))
-    #precision = 0 if (guesses == 0) else hits/guesses
-    #recall = 1 if (answers == 0) else hits/answers
-    #print("Answers:" + str(answers) + " Guesses: " + str(guesses) + " Precision:" + str(precision) + " Recall:" + str(recall))
-    #print(str(gold) + "\n" + str(extractedEntities) + "\n")
-    i += 1
-
-precision, recall = hits/guesses, hits/answers
-fscore = (2 * precision * recall)/(precision + recall)
-
-print("Precision:" + str(hits/guesses) + " Recall:" + str(hits/answers))
-print("F-score:" + str(fscore))
-"""
-
-
-
-
-
-
+def detectRelation(first: Span, second: Span, sentence: Span):
+    return "bruh"
