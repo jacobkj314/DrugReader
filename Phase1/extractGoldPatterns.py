55cc879bee8cfa9be4134929436e568714de7073
jacobkj314 <jacobkj314@gmail.com>
2022-02-10 09:36:06 -0700

    extracted gold patterns

--- Phase1/extractGoldPatterns.py
+++ Phase1/extractGoldPatterns.py
@@ -9,23 +9,35 @@ gold = getGold(partition)
 
 nlp = spacy.load("en_core_web_lg")
 
-labels = dict()
-labels["effect"] = set()
-labels["mechanism"] = set()
-labels["advice"] = set()
-labels["int"] = set()
+labels = ["effect", "mechanism", "advise", "int"]
+
+patterns = dict()
+for label in labels:
+    patterns[label] = set() 
 
 for doc in gold:
     for sentenceText, drugs, interactions in doc:
         sentenceText = re.sub(r"^[^A-Za-z0-9]+|[^A-Za-z0-9]+$", r"", sentenceText)#remove leading/trailing non-alphanumeric characters
-        if len(interactions) != 0:
-            print("\n" + sentenceText + ":")
-        sentence = nlp(sentenceText)[:]
-        for first, second, label in interactions:
-            drug1 = sentence.char_span(drugs[first][0], drugs[first][1])
-            drug2 = sentence.char_span(drugs[second][0], drugs[second][1])
-            if drug1 is None:
-                print("drug1: " + sentenceText[drugs[first][0]: drugs[first][1]])
-            if drug2 is None:
-                print("drug2: " + sentenceText[drugs[second][0]: drugs[second][1]])
-            print(drug1.text + ", " + drug2.text + ": " + extractPattern(drug1, drug2, sentence))
+        #if len(interactions) != 0:
+            #print("\n" + sentenceText + ":")
+        sentenceAsDoc = nlp(sentenceText)
+        drug = [sentenceAsDoc.char_span(start, end) for start, end, _ in drugs]
+
+        for one, two, label in interactions:
+            if label not in labels:
+                print(str(label) + " " + sentenceText)
+                continue
+            #some entities are parsed by spacy into different word boundaries. This check makes sure that we extract gold patterns from entities that spacy can detect
+            if drug[one] is None or drug[two] is None:
+                continue
+            #some sentences are parsed by spacy into two sentences. This loop makes sure that we use the part of the sentence that includes both entities, if possible
+            sentence = None
+            for s in sentenceAsDoc.sents:
+                if drug[one].root in s and drug[two].root in s:
+                    sentence = s
+                    break
+            if sentence is not None:#we can extract!
+                #print(drug[one].text + ", " + drug[two].text + ": " + extractPattern(drug[one], drug[two], sentence))
+                patterns[label].add(extractPattern(drug[one], drug[two], sentence))
+
+pickle.dump(patterns, open("goldPatterns", "wb"))
